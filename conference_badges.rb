f0594f36b617a8107db0fac28e7a089e9986eed5
Jharell Chambers <jharell.chambers@selu.edu>
2019-08-14 00:20:45 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,9 +2,9 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def room_assigninments(speaker, counter)
-  return "Hello, #{speaker}! You'll be assigned to room #{counter}!"
-    attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def room_assigninments(attendee, number)
+  return "Hello, #{attendee}! You'll be assigned to room #{number}!"
+  
 end
 
 
@@ -15,8 +15,8 @@ def batch_badge_creator(array)
    badge_message = []
    
    i = 0 
-    array.each do |speaker|
-      new_arr = badge_maker(speaker)
+    array.each do |attendee|
+      new_arr = badge_maker(attendee)
       badge_message << new_arr
       
     i += 1 
@@ -30,13 +30,12 @@ batch_badge_creator(["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz
 
 
 
-def assign_rooms
-
-  room_message = []
+def assign_rooms(array)
+room_message = []
   
   counter = 1 
-   attendees.each do |speaker|
-   new_arr = room_assigninments(speaker)
+   array.each do |attendee|
+   new_arr = room_assigninments(attendee, counter)
    room_message << new_arr
    
    counter += 1 
@@ -44,5 +43,27 @@ def assign_rooms
   
   return room_message
 end
+
+assign_rooms(["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"])
+
+
+
+
+
+def printer(array)
+  
+  batch_badge_creator(array).each do |attendee|
+    puts attendee
+  end 
+
+  assign_rooms(array).each do |attendee|
+    puts attendee
+end
+
+end
+
+ 
+
+
 
 
